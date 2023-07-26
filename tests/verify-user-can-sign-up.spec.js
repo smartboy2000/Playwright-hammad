4cebe2a0dbcda6a25d5fe2b364f35780c858a8f3
hammad1822 <hammad.10pearls@gmail.com>
2023-07-26 17:08:54 +0500

    first commit

---
+++ tests/verify-user-can-sign-up.spec.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+const { signUp } = require('./playwright-helpers/signup-helpers.js');
+
+test('Verify use can sign up a new account', async ({ page }) => {
+
+    const userSignUp = new signUp(page);
+
+    const randomUsername = Math.random().toString(36).substring(2,7);
+    console.log(randomUsername);
+    const randomPassword = Math.random().toString(36).substring(2,7);
+    console.log(randomPassword);
+
+    await userSignUp.gotoSite();
+    await userSignUp.signUpNewUser(randomUsername, randomPassword);
+    await userSignUp.login(randomUsername,randomPassword);
+    
+    await expect(userSignUp.welcomeText).toHaveText('Welcome ' + randomUsername);
+});
+
+
+
+
+
+
+
+
+
+
+
