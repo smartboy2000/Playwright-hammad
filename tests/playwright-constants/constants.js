542436b9251c75b7273832933fe996049b7da97e
hammad1822 <hammad.10pearls@gmail.com>
2023-08-03 14:39:22 +0500

    Update constant file.

--- tests/playwright-constants/constants.js
+++ tests/playwright-constants/constants.js
@@ -1,5 +1,3 @@
-//const { testData } = require('@playwright/test');
-
 module.exports = {
     user: {
         fullname : 'Jimmyy',
@@ -11,16 +9,3 @@ module.exports = {
     }
 }   
 
-    
-
-// exports.testData = class testData{
-//   // Define an option and provide a default value.
-//   // We can later override it in the config.
-//     fullname = 'Jimmyy' 
-//     country = 'canada'
-//     city = 'Toronto'
-//     creditcard ='4588526658525689'
-//     month = 'July'
-//     year = '1990'
-
-// };
